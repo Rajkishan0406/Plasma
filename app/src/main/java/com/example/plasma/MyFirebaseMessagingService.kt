d1fe6e4591140010c04f72107ce2f91b9dafb82a
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-10-02 13:18:01 -0400

    Message from firebase working great

---
+++ app/src/main/java/com/example/plasma/MyFirebaseMessagingService.kt
@@ -0,0 +1,45 @@
+package com.example.plasma
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    val TAG = "Service"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
+        sendNotification(remoteMessage)
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("message", remoteMessage.notification!!.body!!)
+        startActivity(intent)
+    }
+
+
+
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                .setContentText(remoteMessage.notification?.body)
+                .setContentTitle(remoteMessage.notification?.title)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
