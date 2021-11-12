5f317816c7dac91c59c2da33592a9eaf282326e7
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-11-11 23:19:31 -0500

    profile fragment done

--- app/src/main/java/com/example/plasma/Dashboard/Setting/developerFragment.kt
+++ app/src/main/java/com/example/plasma/Dashboard/Setting/developerFragment.kt
@@ -1,21 +1,30 @@
 package com.example.plasma.Dashboard.Setting
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import com.example.plasma.R
 
 class developerFragment : Fragment() {
 
-
+    lateinit var card : CardView
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_developer, container, false)
 
+        requireActivity().window.statusBarColor = Color.parseColor("#048AD3")
 
+        card = view.findViewById(R.id.contact_card)
+
+        card.setCardBackgroundColor(Color.TRANSPARENT)
+        card.cardElevation = 0F
+        
         return view
     }
 
